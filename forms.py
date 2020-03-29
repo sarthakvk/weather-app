1923174fb48d9d94b5df94c0302a72388be493ce
Sarthakchaudhary13 <sarthakchaudhary13@gmail.com>
2020-03-29 13:05:37 +0530

    recaptcha

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from wtforms import StringField,SubmitField
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms.validators import data_required
 
 
 class myform(FlaskForm):
 
     location = StringField("Enter location",validators=[data_required()])
+    recaptcha = RecaptchaField()
     submit = SubmitField("Submit")
