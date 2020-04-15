5055284c8b29304a8114a78dde223c8f18b1d2b6
Restyled.io <commits@restyled.io>
2020-04-15 01:40:25 +0000

    Restyled by yapf

--- app.py
+++ app.py
@@ -12,14 +12,8 @@ from forms import myform
 
 def weather_json(arr):
     your_darksky_api = "3a01348a0896d1dd67db0fcea5d940e9"
-    url = (
-        "https://api.darksky.net/forecast/"
-        + your_darksky_api
-        + "/"
-        + str(arr[0])
-        + ","
-        + str(arr[1])
-    )
+    url = ("https://api.darksky.net/forecast/" + your_darksky_api + "/" +
+           str(arr[0]) + "," + str(arr[1]))
     jsn = requests.get(url)
 
     result = jsn.json()
@@ -46,7 +40,6 @@ API_KEY = "3a01348a0896d1dd67db0fcea5d940e9"
 
 darksky = DarkSky(API_KEY)
 
-
 geolocator = Nominatim(user_agent="app.py", timeout=10)
 
 app = Flask(__name__)
@@ -54,7 +47,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "sbvfet32vob4q6vr87q6tc32b9nxu98eb213x2v"
 app.config["RECAPTCHA_USE_SSL"] = False
 app.config["RECAPTCHA_PUBLIC_KEY"] = "6Lf65OQUAAAAAP8fZ1lXKlEveRkAHfK8wixgoYpi"
-app.config["RECAPTCHA_PRIVATE_KEY"] = "6Lf65OQUAAAAAG5jbr8g9FqezcWqaFfUbSOjunsP"
+app.config[
+    "RECAPTCHA_PRIVATE_KEY"] = "6Lf65OQUAAAAAG5jbr8g9FqezcWqaFfUbSOjunsP"
 app.config["RECAPTCHA_OPTIONS"] = {"theme": "white"}
 
 
@@ -72,7 +66,10 @@ def index():
             flash("Please enter your location")
             return redirect(url_for("index"))
 
-    return render_template("index.html", form=form, location=location, forcast=forcast)
+    return render_template("index.html",
+                           form=form,
+                           location=location,
+                           forcast=forcast)
 
 
 if __name__ == "__main__":
